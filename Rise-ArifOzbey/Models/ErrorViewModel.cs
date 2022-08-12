f99c195c05bac460ea47bf812544223261385cc1
arif özbey <arifozbey@gmail.com>
2022-08-12 14:53:26 +0300

    Add project files.

---
+++ Rise-ArifOzbey/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Rise_ArifOzbey.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
