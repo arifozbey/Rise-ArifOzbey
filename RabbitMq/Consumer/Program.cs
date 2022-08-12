734cb7a043aa969f2d83f12bed8ca65144349ef6
arif özbey <arifozbey@gmail.com>
2022-08-12 16:13:12 +0300

    kuyruk yapısı publisher ve consumer ayarlandı

---
+++ RabbitMq/Consumer/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+
+                channel.QueueDeclare(queue: "log_queue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine($" message : " + message);
+                };
+
+                channel.BasicConsume(queue: "log_queue",
+                                     noAck: true,
+                                     consumer: consumer);
+
+
+                Console.ReadLine();
+            }
+
+            #region Method2
+            //var logReceiver = new LogReceiver("localhost", "log_queue");
+            //logReceiver.GetLogs();
+            //Console.WriteLine(" Press [enter] to exit.");
+            //    Console.ReadLine();
+            #endregion
+        }
+    }
+}
