d41764eac700acba508fcd61220bda6b9632f770
arif özbey <arifozbey@gmail.com>
2022-12-07 00:16:29 +0300

    db test

--- ConsumerRabbitMQ/Data/ApplicationDbContext.cs
+++ ConsumerRabbitMQ/Data/ApplicationDbContext.cs
@@ -9,7 +9,7 @@ namespace Consumer
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
 
-            optionsBuilder.UseNpgsql("User ID=root;Password=root;Server=192.168.116.89;Port=5432;Database=risearifozbey;Integrated Security=true;Pooling=true;Timeout=15;");
+            optionsBuilder.UseNpgsql("User ID=root;Password=root;Server=192.168.144.89;Port=5432;Database=risearifozbey;Integrated Security=true;Pooling=true;Timeout=15;");
         }
         //Daha fazla bağımlılık olmaması için sadece model katmanı referans alındı.
         public virtual DbSet<KisiModel> KisiModels { get; set; }
